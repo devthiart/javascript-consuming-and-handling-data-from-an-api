70d485fdb316225a73eeb34024d29f52b9e4d44a
devthiart <devthiart@gmail.com>
2023-03-30 15:45:46 -0300

    Class 01

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("Sending hi to a friend!");
+
+function sendMessage() {
+  console.log("Hi! How are you?");
+  console.log("I will send you a request!");
+  console.log("Request received!");
+}
+
+setTimeout(sendMessage, 5000);
+
+console.log("Bye!");
