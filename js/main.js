872a6903440e2fb237b7793216e3572f9acf5845
devthiart <devthiart@gmail.com>
2023-03-30 16:40:34 -0300

    Class 03

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 // CEP = Zip Code in Brazil.
-var consultingCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+const validCEP = '01001000';
+const invalidCEP = '01001';
+const nonexistentCEP = '01001250';
+
+var consultingCEP = fetch(`https://viacep.com.br/ws/${validCEP}/json/`)
+  .then(response => response.json())
+  .then(
+    response => {
+      if(response.erro) {
+        throw Error("CEP don't exist!");
+      } else {
+        console.log(response);
+      }
+    }
+  )
+  .catch(err => console.error(err))
+  .finally(() => console.log('Request Completed.'))
 
 console.log(consultingCEP);
