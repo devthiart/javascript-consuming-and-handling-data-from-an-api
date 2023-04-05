662eaab69a3a91f00428e8cff96619cf4aed71db
devthiart <devthiart@gmail.com>
2023-04-05 15:57:13 -0300

    Class 04

--- js/main.js
+++ js/main.js
@@ -3,18 +3,28 @@ const validCEP = '01001000';
 const invalidCEP = '01001';
 const nonexistentCEP = '01001250';
 
-var consultingCEP = fetch(`https://viacep.com.br/ws/${validCEP}/json/`)
-  .then(response => response.json())
-  .then(
-    response => {
-      if(response.erro) {
-        throw Error("CEP don't exist!");
-      } else {
-        console.log(response);
-      }
-    }
-  )
-  .catch(err => console.error(err))
-  .finally(() => console.log('Request Completed.'))
+async function searchAddress(cep) {
+  try {
+    var consultingCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultingCEPConverted = await consultingCEP.json();
 
-console.log(consultingCEP);
+    if(consultingCEPConverted.erro) {
+      throw Error('CEP does not exist.');
+    }
+  
+    return consultingCEPConverted;
+
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+// // Resolving a group of promises.
+// let ceps = ['01001000', '01001001'];
+// let consultedCeps = ceps.map((value) => searchAddress(value));
+
+// // It can be useful for aggregating the results of multiple promises.
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+// Promise.all(consultedCeps).then(response => console.log(response));
+
+searchAddress(validCEP);
